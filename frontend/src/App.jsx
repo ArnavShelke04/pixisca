75049fa4bfaadf93e89ddd446539e9ed8317d609
ArnavShelke04 <arnavshelke2006@gmail.com>
2026-06-04 20:44:28 +0530

    tried smth for zoom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,26 @@ import ColorPicker from "./components/ColorPicker";
 
 function App() {
   const [color, setColor] = useState("#9b59b6"); // Default color
+  const [zoom,setZoom] = useState(1);
 
+  const handleZoom = (e)=>{
+    if (e.deltaY < 0) {
+      setZoom((currentZoom) => Math.min(currentZoom + 0.1, 4));
+    }
+    else {
+      setZoom((currentZoom) => Math.max(currentZoom - 0.1, 0.5));
+    }
+  }
   return (
     <>
-      <ColorPicker color={color} setColor={setColor} />
-      <PixelArt color={color} />
+      <div onWheel={handleZoom} className="relative w-screen h-screen overflow-clip">
+        <PixelArt color={color}/>
+        <div className="absolute z-10 top-0 left-0" style={{ 
+          transform: `scale(${zoom})`, 
+        }}>
+          <ColorPicker color={color} setColor={setColor}/>
+        </div>
+      </div>
     </>
   );
 }
